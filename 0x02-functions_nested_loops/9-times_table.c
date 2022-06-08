428fbe1478c6a628f5cd6701a7d743c67c0faef9
Bri-hub-del <brianmontana0@gmail.com>
2022-06-08 16:56:03 -0700

    Multiplies the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * times_table - Multiplies the digits in 9 times table
+ *
+ * Return: - Always return 0
+ */
+void times_table(void)
+{
+	int mult;
+
+	for (mult >= 0; mult <= 9; mult++)
+	else
+	(mult >= 1; mult <= 10; mult++)
+	_putchar(mult);
+	_putchar('\n');
+	return (0);
+}
