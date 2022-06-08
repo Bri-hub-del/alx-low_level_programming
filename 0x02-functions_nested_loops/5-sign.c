b56d7f8c169494374d9e535af5227f66b00ecfda
Bri-hub-del <brianmontana0@gmail.com>
2022-06-08 13:55:32 -0700

    Sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of number
+ * @n: sign of the number
+ * Return: 1 when n is greater than 0
+ * Return: 0 when n is zero, -1 n is less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
