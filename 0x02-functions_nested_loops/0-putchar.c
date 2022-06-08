2e3cbfc9cb936a8a124cee899e22c2db120c06ac
Bri-hub-del <brianmontana0@gmail.com>
2022-06-08 10:41:42 -0700

    Creates a file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char l[8] = "_putchar";
+	int a;
+
+	for (a = 0; a < 8; a++)
+	{
+		_putchar(l[a]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
